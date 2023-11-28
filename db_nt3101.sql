edd392ec9757a591ae036201398103645b641b36
Pol556 <21-30169@g.batstate-u.edu.ph>
2023-11-28 20:41:00 +0800

    merge another group

--- db_nt3101.sql
+++ db_nt3101.sql
@@ -620,6 +620,258 @@ CREATE DEFINER=`root`@`localhost` EVENT `update_event_status` ON SCHEDULE EVERY 
 END$$
 
 DELIMITER ;
+CREATE TABLE `products` (
+  `id` int(100) NOT NULL,
+  `ProductName` varchar(255) NOT NULL,
+  `adminId` int(11) NOT NULL,
+  `Price` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `ProductName`, `adminId`, `Price`, `image`) VALUES
+(73, 'Department shirt', 37, '400', 'product-1700985708.png'),
+(75, 'Department tags', 37, '100', 'product-1701146087.png'),
+(76, 'Department Jersy', 37, '750', 'product-1701146212.jpg'),
+(77, 'Department Tickler', 37, '250', 'product-1701146317.png'),
+(79, 'Department Jertsy 2', 37, '1000', 'product-1701153070.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `productsuppliers`
+--
+
+CREATE TABLE `productsuppliers` (
+  `id` int(11) NOT NULL,
+  `supplier` int(11) NOT NULL,
+  `product` int(11) NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `productsuppliers`
+--
+
+INSERT INTO `productsuppliers` (`id`, `supplier`, `product`, `updated_at`, `created_at`) VALUES
+(22, 6, 73, '2023-11-28 05:33:03', '2023-11-28 05:33:03'),
+(26, 9, 75, '2023-11-28 05:35:38', '2023-11-28 05:35:38'),
+(27, 12, 76, '2023-11-28 05:37:28', '2023-11-28 05:37:28'),
+(28, 14, 77, '2023-11-28 05:39:27', '2023-11-28 05:39:27'),
+(29, 15, 79, '2023-11-28 07:32:03', '2023-11-28 07:32:03');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_in`
+--
+
+CREATE TABLE `product_in` (
+  `id` int(11) NOT NULL,
+  `supplier` int(11) NOT NULL,
+  `product` int(11) NOT NULL,
+  `quantity_ordered` int(11) NOT NULL,
+  `quantity_received` int(11) NOT NULL,
+  `quantity_remaining` int(11) NOT NULL,
+  `status` varchar(20) NOT NULL,
+  `batch` varchar(200) NOT NULL,
+  `adminId` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `product_in`
+--
+
+INSERT INTO `product_in` (`id`, `supplier`, `product`, `quantity_ordered`, `quantity_received`, `quantity_remaining`, `status`, `batch`, `adminId`, `created_at`, `updated_at`) VALUES
+(1, 6, 73, 1234, 0, 0, 'pending', '1701157514', 37, '2023-11-28 08:45:14', '2023-11-28 08:45:14'),
+(2, 6, 73, 1234, 0, 0, 'pending', '1701157527', 37, '2023-11-28 08:45:27', '2023-11-28 08:45:27'),
+(3, 6, 73, 1234, 0, 0, 'pending', '1701157540', 37, '2023-11-28 08:45:40', '2023-11-28 08:45:40'),
+(4, 6, 73, 1234, 0, 0, 'pending', '1701158540', 37, '2023-11-28 09:02:20', '2023-11-28 09:02:20'),
+(5, 12, 76, 1234, 0, 0, 'pending', '1701158561', 37, '2023-11-28 09:02:41', '2023-11-28 09:02:41'),
+(6, 14, 77, 5643, 0, 0, 'pending', '1701158561', 37, '2023-11-28 09:02:41', '2023-11-28 09:02:41');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_out`
+--
+
+CREATE TABLE `product_out` (
+  `id` int(11) NOT NULL,
+  `supplier` int(11) NOT NULL,
+  `product` int(11) NOT NULL,
+  `quantity_ordered` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `suppliers`
+--
+
+CREATE TABLE `suppliers` (
+  `id` int(11) NOT NULL,
+  `supplier_name` varchar(191) NOT NULL,
+  `supplier_location` varchar(191) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `adminId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `suppliers`
+--
+
+INSERT INTO `suppliers` (`id`, `supplier_name`, `supplier_location`, `email`, `adminId`) VALUES
+(6, 'Department Shirt Supplier', 'balintawak', 'DepartmentShirtSupplier@gmail.com', 1),
+(9, 'Department tags Supplier', 'levitown', 'DepartmenttagsSupplier@gmail.com222', 1),
+(12, 'Department Jersy Supplier', 'Inosloban', 'DepartmentJersySupplier@gmail.com', 37),
+(14, 'Department Tickler Supplier', 'mataas na kahoy', 'DepartmentTicklerSupplier@gmail.com', 37),
+(15, 'Department Jertsy 2 Supplier', 'Cuenca', 'DepartmentJertsy2Supplier@gmail.com', 37);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `emp` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `password`, `created_at`, `updated_at`, `emp`) VALUES
+(1, 'johnmatthew300@gmail.com', 'johnmatthew', '2023-11-15 11:00:22', '2023-11-26 10:05:23', 1),
+(37, 'kyle300@gmail.com', 'kyle123', '2023-11-24 00:25:33', '2023-11-25 09:32:05', 2),
+(57, 'julyan300@gmail.com', '', '2023-11-26 01:03:39', '2023-11-26 01:03:55', 4),
+(58, 'kian300@gmail.com', '', '2023-11-26 03:02:28', '2023-11-26 09:33:10', 3),
+(62, 'jasmine@gmail.com', '', '2023-11-27 04:09:25', '2023-11-27 04:09:25', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `adminId` (`adminId`);
+
+--
+-- Indexes for table `productsuppliers`
+--
+ALTER TABLE `productsuppliers`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product` (`product`),
+  ADD KEY `supplier` (`supplier`);
+
+--
+-- Indexes for table `product_in`
+--
+ALTER TABLE `product_in`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product_supplier_ibfk_1` (`supplier`),
+  ADD KEY `product_supplier_ibfk_2` (`product`),
+  ADD KEY `adminId` (`adminId`);
+
+--
+-- Indexes for table `suppliers`
+--
+ALTER TABLE `suppliers`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `adminId` (`adminId`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `emp` (`emp`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=82;
+
+--
+-- AUTO_INCREMENT for table `productsuppliers`
+--
+ALTER TABLE `productsuppliers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT for table `product_in`
+--
+ALTER TABLE `product_in`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `suppliers`
+--
+ALTER TABLE `suppliers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=67;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`adminId`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `productsuppliers`
+--
+ALTER TABLE `productsuppliers`
+  ADD CONSTRAINT `productsuppliers_ibfk_1` FOREIGN KEY (`product`) REFERENCES `products` (`id`),
+  ADD CONSTRAINT `productsuppliers_ibfk_2` FOREIGN KEY (`supplier`) REFERENCES `suppliers` (`id`);
+
+--
+-- Constraints for table `product_in`
+--
+ALTER TABLE `product_in`
+  ADD CONSTRAINT `product_in_ibfk_1` FOREIGN KEY (`supplier`) REFERENCES `suppliers` (`id`),
+  ADD CONSTRAINT `product_in_ibfk_2` FOREIGN KEY (`product`) REFERENCES `products` (`id`),
+  ADD CONSTRAINT `product_in_ibfk_3` FOREIGN KEY (`adminId`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `suppliers`
+--
+ALTER TABLE `suppliers`
+  ADD CONSTRAINT `suppliers_ibfk_1` FOREIGN KEY (`adminId`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`emp`) REFERENCES `tbempinfo` (`empid`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
